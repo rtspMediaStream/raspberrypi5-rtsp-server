f411cdfda5ca30a2b86320f76e3f57857f4ff7e9
InHakJeon <ij3512@naver.com>
2024-12-11 17:31:01 +0900

    fix: video stream buffer bug fixing

--- src/VideoCapture.cpp
+++ src/VideoCapture.cpp
@@ -1,11 +1,11 @@
 #include "VideoCapture.h"
 #include <cstring>
+#include <iostream>
 
 void VideoCapture::pushImg(unsigned char* imgPtr, int size) {
 
     if(!isFullBuffer()){
-        std::lock_guard<std::mutex> lock(imgBufferMutex);
-
+        imgBufferMutex.lock();
         if(imgBuffer[tail].first == nullptr){
             imgBuffer[tail].first = new unsigned char[size];
         }else if(imgBuffer[tail].second < size){
@@ -19,6 +19,8 @@ void VideoCapture::pushImg(unsigned char* imgPtr, int size) {
 
         tail = (tail + 1) % buffer_max_size; // tail 위치 갱신
         buffer_size++;
+        std::cout << "push img \n"<<buffer_size;
+        imgBufferMutex.unlock();
     }
 }
 
@@ -27,11 +29,13 @@ std::pair<unsigned char*, int> VideoCapture::popImg() {
         return std::make_pair(nullptr, 0);
     }
 
-    std::lock_guard<std::mutex> lock(imgBufferMutex);
-
+    imgBufferMutex.lock();
     auto ret = imgBuffer[head];
     head = (head + 1) % buffer_max_size; // head 위치 갱신
+
+    std::cout << "pop  img \n"<<buffer_size;
     buffer_size--;
+    imgBufferMutex.unlock();
 
     return ret;
 }
