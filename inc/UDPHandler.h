91ba151a22cabe7606326d12033c53a4bae387d8
Taewon Kim <55828162+kimtae9217@users.noreply.github.com>
2025-01-01 23:35:08 +0900

    docs: Update UDPHandler with Doxygen style
    - write Doxygen .cpp simplified

--- inc/UDPHandler.h
+++ inc/UDPHandler.h
@@ -1,3 +1,18 @@
+/**
+ * @file UDPHandler.h
+ * @brief UDP 소켓 관리 클래스 헤더
+ * @details RTP/RTCP 스트리밍을 위한 UDP 소켓을 관리하는 클래스
+ *          - RTP 소켓 생성 및 관리
+ *          - RTCP 소켓 생성 및 관리
+ *          - 클라이언트 주소 관리
+ * 
+ * @organization rtspMediaStream
+ * @repository https://github.com/rtspMediaStream/raspberrypi5-rtsp-server
+ * 
+ * Copyright (c) 2024 rtspMediaStream
+ * This project is licensed under the MIT License - see the LICENSE file for details
+ */
+
 #ifndef RTSP_UDPHANDLER_H
 #define RTSP_UDPHANDLER_H
 
@@ -7,28 +22,63 @@
 
 class ClientSession;
 
+/**
+ * @class UDPHandler
+ * @brief RTP/RTCP를 위한 UDP 소켓 관리 클래스
+ * @details 클라이언트와의 미디어 스트리밍을 위한 UDP 소켓을 관리하며,
+ *          RTP/RTCP 프로토콜에 필요한 소켓과 주소 정보를 제공한다.
+ */
 class UDPHandler {
 public:
+    /**
+     * @brief UDP 핸들러 생성자
+     * @param client 클라이언트 세션 객체
+     */
     UDPHandler(std::shared_ptr<ClientSession> client);
+
+    /**
+     * @brief 소멸자 - 소켓 리소스 해제
+     */
     ~UDPHandler();
 
+    /**
+     * @brief RTP와 RTCP UDP 소켓을 생성하는 메서드
+     * @return bool 소켓 생성 성공 여부
+     * @details 클라이언트의 RTP/RTCP 포트로 연결할 UDP 소켓을 생성
+     */
     bool CreateUDPSocket();
 
+    /**
+     * @brief RTP 소켓을 반환하는 메서드
+     * @return int& RTP 소켓 디스크립터에 대한 참조
+     */
     int& GetRTPSocket();
+
+    /**
+     * @brief RTCP 소켓을 반환하는 메서드
+     * @return int& RTCP 소켓 디스크립터에 대한 참조
+     */
     int& GetRTCPSocket();
 
+    /**
+     * @brief RTP 주소를 반환하는 메서드
+     * @return sockaddr_in& RTP 주소 구조체에 대한 참조
+     */
     sockaddr_in& GetRTPAddr();
+
+    /**
+     * @brief RTCP 주소를 반환하는 메서드
+     * @return sockaddr_in& RTCP 주소 구조체에 대한 참조
+     */
     sockaddr_in& GetRTCPAddr();
 
 
 private:
-    std::shared_ptr<ClientSession> client;
-
-    int rtpSocket;
-    int rtcpSocket;
-
-    sockaddr_in rtpAddr;
-    sockaddr_in rtcpAddr;
+    std::shared_ptr<ClientSession> client; ///< 클라이언트 세션 객체
+    int rtpSocket;                         ///< RTP 소켓 디스크립터
+    int rtcpSocket;                        ///< RTCP 소켓 디스크립터
+    sockaddr_in rtpAddr;                   ///< RTP 주소 구조체
+    sockaddr_in rtcpAddr;                  ///< RTCP 주소 구조체
 };
 
 #endif //RTSP_UDPHANDLER_H
--- src/UDPHandler.cpp
+++ src/UDPHandler.cpp
@@ -1,3 +1,12 @@
+/**
+ * @file UDPHandler.cpp
+ * @brief UDPHandler 클래스의 구현부
+ * @details UDPHandler 클래스의 멤버 함수를 구현한 소스 파일
+ * 
+ * Copyright (c) 2024 rtspMediaStream
+ * This project is licensed under the MIT License - see the LICENSE file for details
+ */
+
 #include "UDPHandler.h"
 #include "ClientSession.h"
 #include <string>
@@ -7,14 +16,28 @@
 #include <arpa/inet.h>
 #include <sys/socket.h>
 
+/**
+ * @details 소켓 디스크립터를 초기값 -1로 초기화하여 유효하지 않은 소켓임을 표시
+ */
 UDPHandler::UDPHandler(std::shared_ptr<ClientSession> client)
     : client(client), rtpSocket(-1), rtcpSocket(-1) {}
 
+/**
+ * @details 열려있는 소켓이 있다면(디스크립터가 -1이 아닌 경우) 소켓을 닫고 리소스 해제
+ */
 UDPHandler::~UDPHandler() {
     if (rtpSocket != -1) close(rtpSocket);
     if (rtcpSocket != -1) close(rtcpSocket);
 }
 
+/**
+ * @details UDP 소켓 생성 과정:
+ *          1. RTP와 RTCP 소켓 각각 생성
+ *          2. 주소 구조체 초기화 및 설정
+ *             - AF_INET: IPv4 프로토콜
+ *             - SOCK_DGRAM: UDP 소켓 타입
+ *          3. 클라이언트의 IP 주소와 포트 번호로 주소 구조체 설정
+ */
 bool UDPHandler::CreateUDPSocket() {
     rtpSocket = socket(AF_INET, SOCK_DGRAM, 0);
     if (rtpSocket == -1) {
