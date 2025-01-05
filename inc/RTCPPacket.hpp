531f19d2644464f17ffb02af820dce4f6cc41458
Taewon Kim <55828162+kimtae9217@users.noreply.github.com>
2025-01-05 20:51:23 +0900

    docs: Update RTCPPacket with Doxygen style

--- inc/RTCPPacket.hpp
+++ inc/RTCPPacket.hpp
@@ -1,10 +1,16 @@
 /**
  * @file RTCPPacket.hpp
- * @brief rtcp 패킷 정의
- * @details 이 파일은 h264 페이로드의 rtcp 패킷를 정의한 파일입니다.
- * @author InHak Jeon
- * @date 2024/12/31
- * @version 0.0.1
+ * @brief RTCP 패킷 데이터 및 전송 처리 클래스 헤더
+ * @details RTCP(RTP Control Protocol) 패킷의 구조체를 정의하고 전송을 처리하는 클래스
+ *          - RTCP Sender Report 패킷 구조 정의
+ *          - 빅엔디안, 리틀엔디안을 위한 네트워크 바이트 처리
+ *          - UDP 소켓을 통한 패킷 전송
+ * 
+ * @organization rtspMediaStream
+ * @repository https://github.com/rtspMediaStream/raspberrypi5-rtsp-server
+ * 
+ * Copyright (c) 2024 rtspMediaStream
+ * This project is licensed under the MIT License - see the LICENSE file for details
  */
 
 #pragma once
@@ -15,35 +21,61 @@
 #include <arpa/inet.h>
 #include "RTSPServer.h"
 
-#pragma pack(1)
+#pragma pack(1)  ///< 1바이트 정렬로 패딩 없이 메모리에 패킷 구조체 배치
+
+/**
+ * @class RTCPPacket
+ * @brief RTCP 패킷을 생성하고 전송하는 클래스
+ * @details RTCP Sender Report 패킷의 구조를 정의하고, 
+ *          네트워크 바이트 오더 변환과 UDP 소켓을 통한 전송을 처리한다.
+ */
 class RTCPPacket
 {
 public:
+    /**
+     * @brief RTCP 패킷 생성자
+     * @param timestamp RTP 타임스탬프
+     * @param packetCount 전송된 패킷 수
+     * @param octetCount 전송된 총 바이트 수
+     * @param payloadType 페이로드 타입 (H264 또는 OPUS)
+     */
     RTCPPacket(const unsigned int timestamp, unsigned int packetCount, unsigned int octetCount, Protocol payloadType);
+
+    /**
+     * @brief RTCP 패킷을 UDP 소켓으로 전송하는 메서드
+     * @param sockfd UDP 소켓 디스크립터
+     * @param _bufferLen 전송할 버퍼의 크기
+     * @param flags 전송 옵션 플래그
+     * @param to 수신자 주소 정보
+     * @return int64_t 전송된 바이트 수
+     */
     int64_t rtcp_sendto(int sockfd, int64_t _bufferLen, int flags, const sockaddr *to);
+
+    /**
+     * @brief 소멸자
+     */
     ~RTCPPacket() = default;
 
 private:
 #if __BYTE_ORDER == __LITTLE_ENDIAN
-    uint8_t rc : 5;
-    uint8_t p : 1;
-    uint8_t version : 2;
+    uint8_t rc : 5;        ///< 리포트 카운트
+    uint8_t p : 1;         ///< 패딩 비트
+    uint8_t version : 2;   ///< 버전 정보 (2)
 #elif __BYTE_ORDER == __BIG_ENDIAN
-    uint8_t version : 2;
-    uint8_t p : 1;
-    uint8_t rc : 5;
-
+    uint8_t version : 2;   ///< 버전 정보 (2)
+    uint8_t p : 1;         ///< 패딩 비트
+    uint8_t rc : 5;        ///< 리포트 카운트
 #else
 #error "Please fix <bits/endian.h>"
 #endif
-    uint8_t pt;
-    uint16_t length;
-    uint32_t ssrc;
-    uint32_t ntpTimestampMsw;
-    uint32_t ntpTimestampLsw;
-    uint32_t rtpTimestamp;
-    uint32_t senderPacketCount;
-    uint32_t senderOctetCount;
+    uint8_t pt;              ///< 패킷 타입 (200: Sender Report)
+    uint16_t length;         ///< 패킷 길이
+    uint32_t ssrc;           ///< 동기화 소스 식별자
+    uint32_t ntpTimestampMsw; ///< NTP 타임스탬프 MSW
+    uint32_t ntpTimestampLsw; ///< NTP 타임스탬프 LSW
+    uint32_t rtpTimestamp;    ///< RTP 타임스탬프
+    uint32_t senderPacketCount; ///< 전송된 패킷 수
+    uint32_t senderOctetCount;  ///< 전송된 총 바이트 수
 };
 
 #pragma pack()
--- src/RTCPPacket.cpp
+++ src/RTCPPacket.cpp
@@ -1,5 +1,27 @@
+/**
+ * @file RTCPPacket.cpp
+ * @brief RTCPPacket 클래스의 구현부
+ * @details RTCPPacket 클래스의 멤버 함수를 구현한 소스 파일
+ * 
+ * Copyright (c) 2024 rtspMediaStream
+ * This project is licensed under the MIT License - see the LICENSE file for details
+ */
+
 #include "RTCPPacket.hpp"
 #include "Global.h"
+
+/**
+ * @details 
+ *   - RTCP 버전 2로 설정
+ *   - 패킷 타입을 200(SR)으로 설정
+ *   - 패킷 길이를 6으로 설정
+ *   - SSRC를 페이로드 타입으로 설정
+ *   - NTP 타임스탬프 생성 및 설정
+ *   - 네트워크 바이트로 변환하여 설정
+ *   - RTP 타임스탬프 설정
+ *   - 전송된 패킷 수 설정 
+ *   - 전송된 바이트 수 설정 (옥텟 수)
+ */
 
 RTCPPacket::RTCPPacket(const unsigned int timestamp, unsigned int packetCount, unsigned int octetCount, Protocol payloadType)
 {
@@ -18,6 +40,9 @@ RTCPPacket::RTCPPacket(const unsigned int timestamp, unsigned int packetCount, u
     senderOctetCount = htonl(octetCount);
 }
 
+/**
+ * @details RTCP 패킷을 UDP 소켓을 통해 지정된 주소로 전송하는 메서드
+ */
 int64_t RTCPPacket::rtcp_sendto(int sockfd, int64_t _bufferLen, int flags, const sockaddr *to)
 {
     auto sentBytes = sendto(sockfd, this, _bufferLen, flags, to, sizeof(sockaddr));
